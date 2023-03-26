b1c35d6b306eaa96950c48dcab251151422e54f1
NBell-Asher <tashtbell@gmail.com>
2023-03-26 11:38:19 +0100

    Added API integration for country and weather icon

--- src/index.js
+++ src/index.js
@@ -18,18 +18,22 @@ function displayTemp(response){
   console.log(response);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
+let countryElement = document.querySelector("#country");
 let descriptionElement = document.querySelector("#description");
 let dateElemnent = document.querySelector("#date");
+let iconElement = document.querySelector("#weatherIcon");
 
 temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 cityElement.innerHTML = response.data.city;
-//figure out how to return country as well
+countryElement.innerHTML = response.data.country;
 descriptionElement.innerHTML = response.data.condition.description;
 dateElemnent.innerHTML = formatDate(response.data.time*1000);
+iconElement.setAttribute("src", response.data.condition.icon_url);
+iconElement.setAttribute("alt", response.data.condition.icon);
 }
 
 let apiKey = "3c48a60cea5at02a4bc6bf4c51bo5096";
-let city = "Auckland";
+let city = "London";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
