d20d43566b1446de3efe07a259b54e649801c1ce
NBell-Asher <tashtbell@gmail.com>
2023-03-26 11:18:30 +0100

    Added API integration for date

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes <10) {
+  minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", 'Tuesday', "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+
+return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemp(response){
   console.log(response);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
+let dateElemnent = document.querySelector("#date");
 
 temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 cityElement.innerHTML = response.data.city;
 //figure out how to return country as well
 descriptionElement.innerHTML = response.data.condition.description;
+dateElemnent.innerHTML = formatDate(response.data.time*1000);
 }
 
 let apiKey = "3c48a60cea5at02a4bc6bf4c51bo5096";
-let city = "London";
+let city = "Auckland";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
