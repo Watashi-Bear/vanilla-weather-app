7055d190487455607e2b2da08484141877975937
NBell-Asher <tashtbell@gmail.com>
2023-03-27 22:27:42 +0100

    removed console log and amended wind speed conversion to reflect mph

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response){
-  console.log(response);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let countryElement = document.querySelector("#country");
@@ -32,7 +31,7 @@ cityElement.innerHTML = response.data.city;
 countryElement.innerHTML = response.data.country;
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = response.data.temperature.humidity;
-windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+windElement.innerHTML = Math.round(response.data.wind.speed * 2.2369362912);
 dateElemnent.innerHTML = formatDate(response.data.time * 1000);
 iconElement.setAttribute("src", response.data.condition.icon_url);
 iconElement.setAttribute("alt", response.data.condition.icon);
@@ -45,9 +44,9 @@ axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value);
+event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
 }
 
 function searchLocation(position) {
@@ -57,7 +56,6 @@ let lat = position.coords.latitude;
 let apiKey = "3c48a60cea5at02a4bc6bf4c51bo5096";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
- 
 }
 
 function getCurrentLocation(event){
@@ -66,13 +64,13 @@ navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 function displayFarenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
+event.preventDefault();
+let temperatureElement = document.querySelector("#temperature");
   
-  celsiusLink.classList.remove("active");
-  farenheitLink.classList.add("active");
-  let farenheitTemperature = (celsiusTemperature*9)/5+32;
-  temperatureElement.innerHTML = Math.round(farenheitTemperature);
+celsiusLink.classList.remove("active");
+farenheitLink.classList.add("active");
+let farenheitTemperature = (celsiusTemperature*9)/5+32;
+temperatureElement.innerHTML = Math.round(farenheitTemperature);
 }
 
 function displayCelsiusTemperature(event) {
@@ -84,7 +82,6 @@ temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
 let celsiusTemperature = null;
-//fix buttons to then do a current temperature button
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
